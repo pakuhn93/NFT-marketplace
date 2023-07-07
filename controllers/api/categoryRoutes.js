a0355407d4f3b5a3db4ac259c851f306ac244682
pakuhn93 <pakuhn93@gmail.com>
2023-07-07 11:30:42 -0700

    resolved the bug freezeTableName is now true like it's supposed to be

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
-const { Category, Item } = require('../../models');
+const { Category } = require('../../models');
 
 // Endpoint: /api/categories
 
 router.get('/', async (req, res) => {
     try {
-        const categories = await Category.findAll({
-            include: [Item]
-        });
+        const categories = await Category.findAll();
         res.status(200).json(categories);
     } catch (err){
         res.status(500).json(err);
@@ -17,7 +15,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         await Category.create(req.body);
-        res.status(200).json(`'${req.body}' category has been created.`);
+        res.status(200).json(`'${req.body.category_name}' category has been created.`);
     } catch (err){
         res.status(500).json(err);
     }
--- server.js
+++ server.js
@@ -34,13 +34,14 @@ app.use(express.static(join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// need to comment these out if we're going to rebuild models
+// need to comment this out if we're going to rebuild models
 // then set sync to true
 // run it
+// stop server
 // then set to false and uncomment controllers
 // call jeff if things happen bad
 app.use(require('./controllers'));
 
-sequelize.sync({ force: false })
+sequelize.sync({ force: true })
   .then(() => app.listen(3001))
   .catch(err => console.error(err));
