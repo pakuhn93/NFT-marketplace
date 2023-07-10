49b4cbf96bc08a182d47601ab2c1a051dee3534d
pakuhn93 <pakuhn93@gmail.com>
2023-07-10 12:00:03 -0700

    added two home routes: '/' and '/marketplace'

---
+++ controllers/home-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const Item = require('../models/Item');
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('auth');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/marketplace', async (req, res) => {
+    try {
+        const itemData = await Item.findAll();
+        const items = itemData.map((item) => item.get({ plain: true }));
+        res.render('marketplace', { items });
+
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
