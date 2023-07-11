aae92f00ce0a7c9daf48a59b889d19d98ed6f6c0
pakuhn93 <pakuhn93@gmail.com>
2023-07-11 14:08:48 -0700

    making sure i committed

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Item = require('../models/Item');
 
 // the home endpoint
 router.get('/', async (req, res) => {
